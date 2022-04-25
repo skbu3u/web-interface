3afe099c881825346397936a5be51c1260cec347
xansochi <ghost_p@mail.ru>
2022-04-25 20:29:14 +0300

    add smart api search component

--- src/components/Apiconst/apiConstants.jsx
+++ src/components/Apiconst/apiConstants.jsx
@@ -9,3 +9,6 @@ export const API_CONSUM_GET = 'http://62.113.99.92:8000/consumables?skip=0';
 export const API_EQUIP_ADD = 'http://62.113.99.92:8000/equipments';
 export const API_PARTS_ADD = 'http://62.113.99.92:8000/parts';
 export const API_CONSUM_ADD = 'http://62.113.99.92:8000/consumables';
+export const API_SEARCH_EQ = 'http://62.113.99.92:8000/search/equipments';
+export const API_SEARCH_PARTS = 'http://62.113.99.92:8000/search/parts';
+export const API_SEARCH_CONSUMABLES = 'http://62.113.99.92:8000/search/consumables';
--- src/components/Pages/Сonsumables/ConsumablesPage.jsx
+++ src/components/Pages/Сonsumables/ConsumablesPage.jsx
@@ -1,119 +1,69 @@
-/* eslint-disable no-shadow */
 /* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState, useEffect } from 'react';
-import { FcEngineering, FcDeleteDatabase } from 'react-icons/fc';
-import { ToastContainer } from 'react-toastify';
-import ModalAddCons from '../../UI/ModalAddCons/ModalAddCons';
-import { API_CONSUM_GET } from '../../Apiconst/apiConstants';
+import { injectStyle } from 'react-toastify/dist/inject-style';
+import ConsumablesToastContainer, { ConsumablesButton, ConsumablesSearch } from './ConsumablesData';
+import ModalAddParts from '../../UI/ModalAddParts/ModalAddParts';
+import { API_CONSUM_GET, API_SEARCH_CONSUMABLES } from '../../Apiconst/apiConstants';
+import ConsumablesTable from './ConsumablesTable';
 
-export default function ConsumablesPage() {
+export default function PartsPage() {
   const [userdata, setUserData] = useState([]);
+  const [query, setQuery] = useState('c');
   const [open, setOpen] = useState(false);
-
+  injectStyle();
   useEffect(() => {
     fetch(API_CONSUM_GET)
       .then((response) => response.json())
       .then((data) => (setUserData(data)));
   }, []);
-  return (
-    <>
-      <ToastContainer
-        position="bottom-center"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <div className="flex flex-col">
-        <div className="px-4 md:px-10 py-4 md:py-7 bg-gray-100 rounded-tl-lg rounded-tr-lg">
-          <div className="sm:flex items-center justify-between">
-            <p className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold leading-normal text-gray-800">Расходные материалы</p>
-            <div>
-              <button
-                type="button"
-                className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2
-                      bg-red-800 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                onClick={() => setOpen(true)}
-              >
-                Добавить расходный материал
-              </button>
-              <ModalAddCons open={open} setOpen={setOpen} />
-            </div>
-          </div>
-        </div>
-        <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table className="min-w-full divide-y divide-gray-200">
-                <thead className="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      className="px-12 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Запчасть
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-12 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Совместимость
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-12 py-1 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    >
-                      Статус
-                    </th>
 
-                    <th
-                      scope="col"
-                      className="px-12 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                    />
-                  </tr>
-                </thead>
-                <tbody className="bg-white divide-y divide-gray-200">
-                  {userdata.map((userdata) => (
-                    <tr key={userdata.id}>
-                      <td className="px-4 py-2 whitespace-nowrap">
-                        <div className="flex items-center">
-                          <div className="flex-shrink-0 h-10 w-10">
-                            <FcEngineering className="h-8 w-8 rounded-full pt-2 text-red-900" alt="" />
-                          </div>
-                          <div className="ml-4">
-                            <div className="text-sm font-medium text-gray-900">{userdata.name}</div>
-                            <div className="text-sm text-gray-500">{userdata.id}</div>
-                          </div>
-                        </div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <div className="text-sm text-gray-900">{userdata.compatibility}</div>
-                        <div className="text-sm text-gray-500">текст</div>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap">
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                          В обороте
-                        </span>
-                      </td>
-                      <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">текст</td>
-                      <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                        <FcDeleteDatabase
-                          className="h-6 w-6 contrast-100 hover:contrast-50"
-                          aria-hidden="true"
-                        />
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
+  useEffect(() => {
+    fetch(`${API_SEARCH_CONSUMABLES}/${query}`)
+      .then((response) => response.json())
+      .then((data) => (setUserData(data)));
+  }, [query]);
+  if (userdata.detail) {
+    return (
+      <>
+        <ConsumablesToastContainer />
+        <div className="flex flex-col">
+          <div className="px-4 bg-gray-100 rounded-tl-lg rounded-tr-lg">
+            <div className="flex items-center justify-between">
+              <p className="lg:text-2xl font-bold leading-normal text-gray-800 pr-8">Расходные материалы</p>
+              <ConsumablesSearch query={query} setQuery={setQuery} />
+              <div>
+                <ConsumablesButton open={open} setOpen={setOpen} />
+                <ModalAddParts open={open} setOpen={setOpen} />
+              </div>
             </div>
           </div>
+          <div className="md:px-10 md:py-7 rounded-tl-lg rounded-tr-lg w-full">
+            Ничего не найдено
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
+  if (userdata[0]) {
+    return (
+      <>
+        <ConsumablesToastContainer />
+        <div className="flex flex-col">
+          <div className="px-4 bg-gray-100 rounded-tl-lg rounded-tr-lg">
+            <div className="flex items-center justify-between">
+              <p className="lg:text-2xl font-bold leading-normal text-gray-800 pr-8">Оборудование</p>
+              <ConsumablesSearch query={query} setQuery={setQuery} />
+              <div>
+                <ConsumablesButton open={open} setOpen={setOpen} />
+                <ModalAddParts open={open} setOpen={setOpen} />
+              </div>
+            </div>
+          </div>
+          <div className="md:px-10 md:py-7 rounded-tl-lg rounded-tr-lg w-full">
+            <ConsumablesTable data={userdata} />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
